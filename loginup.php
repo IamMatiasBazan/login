eda16d8b6511a158aecee28d8ad0bfb8a0dcc50d
[DMatias <[Dbazanmatias2004@gmail.com>
2022-10-05 09:33:05 -0300

    Arreglando las variables de PHP

--- loginup.php
+++ loginup.php
@@ -1,16 +1,13 @@
 <?php 
   include("database/abrir_conexion.php");
 
-  $btn = $_POST['btn-crear-cuenta'];
-  $correo = $_POST['correo'];
-  $password = $_POST['contrasenia'];
 
-  if(isset($btn)) {
-      if($correo == "" || $password == "") {
+  if(isset($_POST['btn-crear-cuenta'])) {
+      if($_POST['correo'] == "" || $_POST['contrasenia'] == "") {
           echo "Rellene los campos";
       } else {
           mysqli_query($conexion, "INSERT INTO $tablaSecion(correo, passwor) 
-          VALUES ('$correo','$password')");
+          VALUES ('$_POST[correo]','$_POST[contrasenia]')");
           echo '<div class="alert alert-success" role="alert">
                   Cuenta creada con exito. <a href="../login.html" class="alert-link">INICIA SECION</a>
                 </div>';
