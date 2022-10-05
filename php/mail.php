3ea15711a150079beb6fa3cdabc2dac48b42e946
[DMatias <[Dbazanmatias2004@gmail.com>
2022-10-05 12:51:08 -0300

    Agregando el cogigo para que llege un email y validar el formulario

--- php/mail.php
+++ php/mail.php
@@ -1,3 +1,28 @@
 <?php 
+$para  = 'bazanmatias2004@gmail.com' . ', '; // atención a la coma
+//$para .= 'wez@example.com';
 
+// título
+$título = 'Gracias por registrarte';
+
+// mensaje
+$mensaje = '
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Validación del correo eléctronico</title>
+</head>
+<body>
+  <p>Tu código de verificación es:</p>
+  <h2>200231</h2>
+</body>
+</html>
+';
+
+// Para enviar un correo HTML, debe establecerse la cabecera Content-type
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Enviarlo
+mail($para, $título, $mensaje, $cabeceras);
 ?>
