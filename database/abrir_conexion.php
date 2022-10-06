1a0cb6d3b4f3e908bafd20739a151d85b1379947
[DMatias <[Dbazanmatias2004@gmail.com>
2022-10-06 20:57:47 -0300

    Agregando los datos del Hosting a las base de datos

--- database/abrir_conexion.php
+++ database/abrir_conexion.php
@@ -1,14 +1,12 @@
 <?php
-    $host="127.0.0.1";
-    $port=3306;
-    $socket="";
-    $user="root";
-    $password="";
-    $dbname="login";
+    $host="sql100.epizy.com";
+    $user="epiz_32740930";
+    $password="uxX5As5Kf20jog";
+    $dbname="epiz_32740930_login";
 
     $tablaSecion = 'secion';
 
-    $conexion = new mysqli($host, $user, $password, $dbname, $port, $socket);
+    $conexion = new mysqli($host, $user, $password, $dbname);
     
     if ($conexion -> connect_errno) {
         echo "Nuestro sitio esta experimentando fallos";
